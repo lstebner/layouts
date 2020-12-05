6466bdb31305a5ec333c415e3a71bc48e343ff3e
Luke Stebner <luke.stebner@gmail.com>
2020-12-05 12:09:02 -0800

    refactor: updating roadkit macropad to reference global keys

--- layouts/roadkit-macropad.js
+++ layouts/roadkit-macropad.js
@@ -1,24 +1,21 @@
 const keys = require("../helpers/keys")
 const roadkit = require("../keyboards/roadkit")
 
-const layer1 = [
-  [keys.num7, keys.num8, keys.num9, keys.kp_plus],
-  [keys.num4, keys.num5, keys.num6, keys.kp_minus],
-  [keys.num1, keys.num2, keys.num3, keys.backspace],
-  [keys.kp_dot, keys.kp_0, keys.raise, keys.kp_equal],
+const layers = {}
+
+layers[0] = [
+  [num7, num8, num9, kp_plus],
+  [num4, num5, num6, kp_minus],
+  [num1, num2, num3, backspace],
+  [kp_dot, kp_0, raise, kp_equal],
 ]
 
-const layer2 = [
-  [keys.esc, keys.xxx, keys.xxx, keys.kp_asterisk],
-  [keys.xxx, keys.xxx, keys.xxx, keys.kp_slash],
-  [keys.xxx, keys.xxx, keys.xxx, keys.xxx],
-  [keys.xxx, keys.xxx, keys.xxx, keys.xxx],
+layers._RAISE = [
+  [esc, xxx, xxx, kp_asterisk],
+  [xxx, xxx, xxx, kp_slash],
+  [xxx, xxx, xxx, xxx],
+  [xxx, xxx, xxx, xxx],
 ]
-
-const layers = {
-  0: layer1,
-  _RAISE: layer2,
-}
 
 module.exports = {
   board: roadkit,
