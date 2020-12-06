1bfa3c58cbb3bc8a836cda58754ecc01aed94230
Luke Stebner <luke.stebner@gmail.com>
2020-12-05 17:03:50 -0800

    refactor: rewriting groups/blanks to be generated so it is easier to maintain

--- layouts/groups/blanks.js
+++ layouts/groups/blanks.js
@@ -1,28 +1,32 @@
 require("../../helpers/keys")
 
-module.exports = {
-  x1: [xxx],
-  x2: [xxx, xxx],
-  x3: [xxx, xxx, xxx],
-  x4: [xxx, xxx, xxx, xxx],
-  x5: [xxx, xxx, xxx, xxx, xxx],
-  x6: [xxx, xxx, xxx, xxx, xxx, xxx],
-  x7: [xxx, xxx, xxx, xxx, xxx, xxx, xxx],
-  x8: [xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx],
-  x9: [xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx],
-  x10: [xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx],
-  x11: [xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx],
-  x12: [xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx, xxx],
-  _1: [___],
-  _2: [___, ___],
-  _3: [___, ___, ___],
-  _4: [___, ___, ___, ___],
-  _5: [___, ___, ___, ___, ___],
-  _6: [___, ___, ___, ___, ___, ___],
-  _7: [___, ___, ___, ___, ___, ___, ___],
-  _8: [___, ___, ___, ___, ___, ___, ___, ___],
-  _9: [___, ___, ___, ___, ___, ___, ___, ___, ___],
-  _10: [___, ___, ___, ___, ___, ___, ___, ___, ___, ___],
-  _11: [___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___],
-  _12: [___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___],
+let blanks = {}
+let amount = 17
+let xKey, _Key
+
+for (let i = 1; i <= amount; i++) {
+  xKey = `x${i}`
+  _Key = `_${i}`
+
+  blanks[xKey] = []
+  blanks[_Key] = []
+
+  for (let k = 0; k < i; k++) {
+    blanks[xKey].push(xxx)
+    blanks[_Key].push(___)
+  }
 }
+
+// the above loop generates exported variables for rows like such:
+//
+//  x1: [xxx],
+//  x2: [xxx, xxx],
+//  x3: [xxx, xxx, xxx],
+//  _1: [___],
+//  _2: [___, ___],
+//  _3: [___, ___, ___],
+//
+//  they can be imported by name, and destructured for use mid-row,
+//  or used as an entire row as is
+
+module.exports = blanks
