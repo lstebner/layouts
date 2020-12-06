9d7d6e65b192ff61d67ee07bf00bcb41ce973376
Luke Stebner <luke.stebner@gmail.com>
2020-12-05 17:40:08 -0800

    feat: adding CustomKey helper for layouts

--- layouts/roadkit-macropad.js
+++ layouts/roadkit-macropad.js
@@ -1,14 +1,17 @@
 const keys = require("../helpers/keys")
 const roadkit = require("../keyboards/roadkit")
 const { x2, x3, x4 } = require("./groups/blanks")
+const CustomKey = require("./helpers/CustomKey")
 
 const layers = {}
+
+const customRaiseKey = CustomKey("MY_RAISE_KEY", "MO(_RAISE)")
 
 layers[0] = [
   [num7, num8, num9, kp_plus],
   [num4, num5, num6, kp_minus],
   [num1, num2, num3, backspace],
-  [kp_dot, kp_0, raise, kp_equal],
+  [kp_dot, kp_0, customRaiseKey, kp_equal],
 ]
 
 layers._RAISE = [
